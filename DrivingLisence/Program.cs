70e0feba4041ff0f8d49d385693611522a8a3977
Kristo Toomsoo <kristo@lteed.ee>
2022-03-01 15:46:37 +0200

    Control Flow

---
+++ DrivingLisence/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DrivingLisence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja sünniaastat
+            //programm arvutab kasutaja vanust
+            //kui kasutaja on noorem, kui 18, siis programm kuvab konsoolis
+            //"oled liiga noor, et juhilube saada";
+            //kui kasutaja on vanem, kui 18, siis programm kuvab konsoolis
+            //"oled piisavalt vana, et juhilube saada";
+            //kui kasutaja on 18, siis programm kuvab konsoolis
+            //"Palju õnne! Nüüd sa saad juhilube taotleda"
+
+
+            Console.WriteLine("Palun sisesta oma sünniaasta");
+            int YearOfBirth = Convert.ToInt32(Console.ReadLine());
+            int UserAge = 2022 - YearOfBirth;
+
+            if (UserAge > 18)
+            {
+                Console.WriteLine("Oled piisavalt vana juhilubadeks");
+            }
+            else if (UserAge < 18)
+            {
+                Console.WriteLine("Oled liiga noor juhilubadeks");
+            }
+            else
+            {
+                Console.WriteLine("Palju õnne, võid juhilube taotleda");
+            }
+
+            Console.WriteLine("Kena päeva!");
+
+        }
+    }
+}
---
+++ UserAge/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UserAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja vanust;
+            //programm arvutab kasutaja sünniaastat;
+            //programm kuvab sünniaastat konsoolis
+            //"oled sündinud aastal {yearOfBirth}"
+
+            Console.WriteLine("Kui vana sa oled?");
+            int UserAge = Convert.ToInt32(Console.ReadLine());
+            int YearOfBirth = 2021 - UserAge;
+
+
+            Console.WriteLine($"Oled sündinud aastal {YearOfBirth}");
+        }
+    }
+}
